8b914e8ac7b26650521d4def7fdbe0fd1bce2b43
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-04-22 23:30:20 -0300

    Testing load of dynamic content in one tab, Success\!

--- javascripts/behavior.js
+++ javascripts/behavior.js
@@ -1,3 +1,13 @@
 $(function(){
-    
+    $('[aria-controls="Hudson"]').click(function(e){
+                       var $this = $(this),
+                           url = $this.attr('href'),
+                           targ = $this.attr('data-target');
+        $.get(url, function(data){
+            $(targ).html(data);
+        });
+        $this.tab('show');
+        return false;
+                           
+                       })
 })
