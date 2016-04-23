ee8df75c8959cbdceb158ffebb0c74cac3dc0ca5
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-04-23 01:00:37 -0300

    Start using ajax calls to load each tab

--- javascripts/behavior.js
+++ javascripts/behavior.js
@@ -1,13 +1,12 @@
 $(function(){
-    $('[aria-controls="Hudson"]').click(function(e){
+    $('[role="tab"]').click(function(e){ // Selecting each anchor element of tabs.
                        var $this = $(this),
                            url = $this.attr('href'),
                            targ = $this.attr('data-target');
-        $.get(url, function(data){
+        $.get(url, function(data){ // Get the html data
             $(targ).html(data);
         });
-        $this.tab('show');
+        $this.tab('show'); // Show the tab.
         return false;
-                           
-                       })
+                       });
 })
