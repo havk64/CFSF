aeb01734766e0a2b0f528bdd5a87e24b8e9df8e8
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-07-01 22:04:09 +0000

    Adding hostname.php

---
+++ hostname.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>
+	Welcome to my Load Balancer landing page!
+</title>
+<body>
+<?php
+echo "<p>" . exec("hostname") . "<p>\n";
+?>
+</body>
+</html>
